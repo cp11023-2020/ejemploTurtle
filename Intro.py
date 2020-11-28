6bff6d0b1d752910edc1b7b2bd19e2a2c97ab13e
cp11023-2020 <giovanniprogramador2020@gmail.com>
2020-11-28 12:30:01 -0600

    agregue score y centrar pelota

--- Intro.py
+++ Intro.py
@@ -1,10 +1,11 @@
 import turtle
 import winsound
 ventana = turtle.Screen()
-ventana.setup(0.6,0.4)
-print('ancho canv',ventana.canvwidth)
+#ventana.setup(0.6,0.4)
+ventana.setup(800,400)
+#print('ancho canv',ventana.canvwidth)
 anchoJuego= ventana.canvwidth-40
-print('ancho:',anchoJuego)
+#print('ancho:',anchoJuego)
 #ventana.bgcolor("black")
 ventana.title("Mi primer ejemplo con Turtle")
 ventana.tracer(1)
@@ -29,26 +30,26 @@ jugador2.shapesize(1,5)
 
 #funciones para el movimiento de los jugadores
 def player1sube():
-    print(ventana.canvheight)
-    print(jugador1.ycor())
+    #print(ventana.canvheight)
+    #print(jugador1.ycor())
     if jugador1.ycor()+140<=ventana.canvheight:
         jugador1.fd(20)
 
 def player1baja():
-    print(ventana.canvheight)
-    print(jugador1.ycor())
+    #print(ventana.canvheight)
+    #print(jugador1.ycor())
     if jugador1.ycor()-130 >= -ventana.canvheight:
         jugador1.bk(20)
 
 def player2sube():
-    print(ventana.canvheight)
-    print(jugador2.ycor())
+    #print(ventana.canvheight)
+    #print(jugador2.ycor())
     if jugador2.ycor()+140<=ventana.canvheight:
         jugador2.fd(20)
 
 def player2baja():
-    print(ventana.canvheight)
-    print(jugador2.ycor())
+    #print(ventana.canvheight)
+    #print(jugador2.ycor())
     if jugador2.ycor()-130 >= -ventana.canvheight:
         jugador2.bk(20)
 
@@ -57,11 +58,18 @@ pelota= turtle.Turtle()
 pelota.color("red")
 pelota.shape("circle")
 pelota.shapesize(2)
+pelota.speed(0)
 pelota.dx = 1
 pelota.dy = 1
 
-#score = turtle.Turtle()
-#score.write("jugardor 1: 0   jugador 2: 0 ", align="center", font=("courier",20,"bold"))
+#score
+score = turtle.Turtle()
+score.penup()
+score.hideturtle()
+score.goto(0,ventana.canvheight/2+10)
+score.write("jugardor 1: 0   jugador 2: 0 ", align="center", font=("courier",20,"bold"))
+score.jugador1=0
+score.jugador2=0
 
 #teclado paraa el movimiento de los jugadores
 ventana.onkeypress(player1sube, "w")
@@ -85,6 +93,21 @@ while not gameOver:
         pelota.dy *=-1
         #winsound.Beep(1000, 50)
 
+    # cuando no alcanza a rebotar en el jugador1
+    if pelota.xcor() < -anchoJuego-30 :
+        pelota.goto(0,0)
+        score.jugador2 +=1
+        score.clear()
+        score.write(f"jugardor 1: {score.jugador1}   jugador 2: {score.jugador2} ",
+                    align="center", font=("courier", 20, "bold"))
+
+    #cuando no alcanza a rebotar en el jugador2
+    if pelota.xcor() > anchoJuego+30:
+        pelota.goto(0,0)
+        score.jugador1 += 1
+        score.clear()
+        score.write(f"jugardor 1: {score.jugador1}   jugador 2: {score.jugador2} ",
+                    align="center", font=("courier", 20, "bold"))
     #rebote en el jugador1
     if pelota.xcor() == -anchoJuego + 30:
         if jugador1.ycor() - 100 <= pelota.ycor() <= jugador1.ycor() + 100:
